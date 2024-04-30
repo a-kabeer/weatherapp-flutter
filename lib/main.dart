e1b62ffc90bbca3f5383e27804df099f05fcf945
a-kabeer <a.kabeer1991@gmail.com>
2024-04-30 23:00:21 +0500

    Weather App

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:weather/weather.dart';
-import 'package:http/http.dart' as http;
 import 'package:weatherv2/screen/splash_screen.dart';
-import 'package:weatherv2/screen/weather/weather_view.dart';
 
 void main() => runApp(MyApp());
 
