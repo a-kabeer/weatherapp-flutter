31a0f9737070f534f907a23fc671b364c0142b4f
a-kabeer <a.kabeer1991@gmail.com>
2024-05-01 11:47:26 +0500

    Weather App

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:lottie/lottie.dart';
 import 'package:flutter/material.dart';
 import 'package:weatherv2/screen/weather/weather_view.dart';
 
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(seconds: 5), () {
+    Timer(Duration(seconds: 7), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
@@ -28,11 +28,30 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      body: Center(
-        child: Image.asset(
-          'assets/images/weather.png',
-          width: 200,
-          height: 200,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(),
+              Lottie.asset('assets/json/app_logo.json'),
+              Text(
+                'Special Thanks to\nSir Bilal Rehman Khan & Sir Huzaifa\n\nPowered By\nAbdul Kabeer',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+          // child: Image.asset(
+          //   'assets/images/weather.png',
+          //   width: 200,
+          //   height: 200,
+          // ),
         ),
       ),
     );
--- lib/screen/weather/weather_view.dart
+++ lib/screen/weather/weather_view.dart
@@ -19,23 +19,46 @@ class _WeatherScreenState extends State<WeatherScreen> {
   bool isLoading = true;
   String? imageUrl;
   String? mainCondition;
+  String? mainDayCondition;
+  String? mainNightCondition;
+
+  bool nightTime = false;
 
   @override
   void initState() {
     super.initState();
-    getCityName();
+    _getCityName();
     updateDateTime();
   }
 
   void updateDateTime() {
     final now = DateTime.now();
     setState(() {
-      day = DateFormat('EEEE').format(now); // Monday, Tuesday, etc.
+      day = DateFormat('EEEE').format(now);
     });
   }
 
-  Future<void> getCityName() async {
+  Future<void> _getCityName() async {
     try {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          setState(() {
+            error = 'Location permission denied.';
+          });
+          return;
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        setState(() {
+          error =
+              'Location permissions are permanently denied, we cannot request permissions.';
+        });
+        return;
+      }
+
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
       String apiKey = '64b5c8fe1b4f4d39aaa6d43e65083542';
@@ -47,23 +70,18 @@ class _WeatherScreenState extends State<WeatherScreen> {
         var data = jsonDecode(response.body);
         var city = data['results'][0]['components']['city'];
         String cityName = city.split(' ')[0];
-        jsonData(cityName);
-
-        // print('City: $cityName');
+        _jsonData(cityName);
       }
-      // else {
-      //   print(
-      //       'Failed to fetch city name with status code: ${response.statusCode}');
-      // }
     } catch (e) {
-      print('Failed to fetch city name: $e');
+      setState(() {
+        error = 'Failed to fetch city name: $e';
+      });
     }
   }
 
-  getWeatherAnimation(String? mainCondition) {
-    print(mainCondition);
-    if (mainCondition == null) return 'assets/json/sunny.json';
-    switch (mainCondition.toLowerCase()) {
+  getWeatherDayAnimation(String? mainDayCondition) {
+    if (mainDayCondition == null) return 'assets/json/sunny.json';
+    switch (mainDayCondition.toLowerCase()) {
       case 'clouds':
       case 'mist':
       case 'haze':
@@ -84,8 +102,31 @@ class _WeatherScreenState extends State<WeatherScreen> {
     }
   }
 
-  jsonData(String cityName) async {
-    setState(() => isLoading = true); // Set loading to true
+  getWeatherNightAnimation(String? mainNightCondition) {
+    if (mainNightCondition == null) return 'assets/json/night.json';
+    switch (mainNightCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'haze':
+      case 'smoke':
+      case 'dust':
+      case 'fog':
+        return 'assets/json/ncloudy.json';
+      case 'rainy':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/json/nrainy.json';
+      case 'thunderstorm':
+        return 'assets/json/nthunder.json';
+      case 'clear':
+        return 'assets/json/night.json';
+      default:
+        return 'assets/json/night.json';
+    }
+  }
+
+  _jsonData(String cityName) async {
+    setState(() => isLoading = true);
 
     String key = '81917c0d92842ca32dde2a7a09de1327';
     var url = Uri.parse(
@@ -95,30 +136,61 @@ class _WeatherScreenState extends State<WeatherScreen> {
     if (response.statusCode == 200) {
       setState(() {
         weatherData = jsonDecode(response.body);
-        isLoading = false; // Set loading to false
+        isLoading = false;
         imageUrl =
             'http://openweathermap.org/img/wn/${weatherData['weather'][0]['icon']}.png';
         mainCondition = weatherData['weather'][0]['main'];
+        nightTime = isNightTime(weatherData);
+        
       });
     } else {
       setState(() {
         error = 'Failed to load data';
+        isLoading = false;
       });
     }
+  }
+
+  bool isNightTime(Map<String, dynamic> weatherData) {
+    DateTime nowUtc = DateTime.now().toUtc();
+
+    int timezoneOffset = weatherData['timezone'];
+    DateTime localCurrentTime = nowUtc.add(Duration(seconds: timezoneOffset));
+
+    int sunriseUtcSeconds = weatherData['sys']['sunrise'];
+    int sunsetUtcSeconds = weatherData['sys']['sunset'];
+    DateTime localSunriseTime = DateTime.fromMillisecondsSinceEpoch(
+            sunriseUtcSeconds * 1000,
+            isUtc: true)
+        .add(Duration(seconds: timezoneOffset));
+    DateTime localSunsetTime = DateTime.fromMillisecondsSinceEpoch(
+            sunsetUtcSeconds * 1000,
+            isUtc: true)
+        .add(Duration(seconds: timezoneOffset));
+
+    return localCurrentTime.isBefore(localSunriseTime) ||
+        localCurrentTime.isAfter(localSunsetTime);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: RefreshIndicator(
-        onRefresh: getCityName,
+        onRefresh: () {
+          setState(() {
+            _getCityName();
+          });
+          return _getCityName();
+        },
         child: Container(
           height: double.infinity,
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
-              colors: [Colors.blue, Colors.lightBlueAccent],
+              colors: nightTime
+                  ? [Colors.indigo, Colors.black]
+                  : [Colors.blue, Colors.lightBlueAccent],
             ),
           ),
           child: SafeArea(
@@ -132,6 +204,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
                               style: const TextStyle(
                                   color: Colors.white, fontSize: 18)))
                       : SingleChildScrollView(
+                          physics: AlwaysScrollableScrollPhysics(),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
@@ -181,8 +254,11 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                   ),
                                 ),
                               Center(
-                                  child: Lottie.asset(
-                                      getWeatherAnimation(mainCondition))),
+                                  child: nightTime
+                                      ? Lottie.asset(getWeatherNightAnimation(
+                                          mainNightCondition))
+                                      : Lottie.asset(getWeatherDayAnimation(
+                                          mainDayCondition))),
                               SizedBox(height: 10),
                               if (weatherData.isNotEmpty &&
                                   weatherData['wind'] != null)
@@ -217,6 +293,21 @@ class _WeatherScreenState extends State<WeatherScreen> {
                                   SizedBox(width: 10),
                                   Text(
                                       'Cloudiness: ${weatherData['clouds']['all']}%',
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.white)),
+                                ],
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  Icon(
+                                      nightTime
+                                          ? Icons.nightlight_round
+                                          : Icons.wb_sunny,
+                                      color: Colors.white,
+                                      size: 24),
+                                  SizedBox(width: 10),
+                                  Text(nightTime ? 'Night' : 'Day',
                                       style: TextStyle(
                                           fontSize: 18, color: Colors.white)),
                                 ],
